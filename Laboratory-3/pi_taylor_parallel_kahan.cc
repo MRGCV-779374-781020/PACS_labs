ac025924b3ed4e2636b4e3c2f4d00d932fc1f5e5
pablogvrcia <pablo2000zgz@gmail.com>
2023-10-17 18:31:33 +0200

    [kahan]

--- Laboratory-3/pi_taylor_parallel_kahan.cc
+++ Laboratory-3/pi_taylor_parallel_kahan.cc
@@ -7,14 +7,45 @@
 #include <utility>
 #include <vector>
 
-using my_float = long double;
+using my_float = float;
 
 void
 pi_taylor_chunk(std::vector<my_float> &output,
         size_t thread_id, size_t start_step, size_t stop_step) {
+    my_float sum = 0.0;
+    my_float c = 0.0;
+    my_float aux, y, t;
+    int sign = start_step & 0x1 ? -1 : 1;
 
+    for (size_t i = start_step; i < stop_step; i++) {
+        aux = sign / static_cast<my_float>(2 * i + 1);
+        sign *= -1;
+        y = aux - c;
+        t = sum + y;
+        c = (t - sum) - y;
+        sum = t;
+    }
 
+    output[thread_id] = 4*sum;
+}
 
+my_float
+kahan_sum(std::vector<my_float> input)
+{
+    my_float sum = 0.0;
+    my_float c = 0.0;
+    my_float aux, y, t;
+
+    std::vector<my_float>::iterator it;
+
+    for (it = input.begin(); it != input.end(); it++) {
+        y = *it - c;
+        t = sum + y;
+        c = (t - sum) - y;
+        sum = t;
+    }
+
+    return sum;
 }
 
 std::pair<size_t, size_t>
@@ -46,7 +77,23 @@ int main(int argc, const char *argv[]) {
     my_float pi;
 
     // please complete missing parts
+    // kahan summation algorithm split the sum in {thread} parts
+    // and then sum the partial sums
+    std::vector<std::thread> thread_pool;
 
+    std::vector<my_float> partial_results(threads);
+
+    for (size_t i = 0; i < threads; i++) {
+        size_t start_step = i*(steps/threads);
+        size_t stop_step = (i+1)*(steps/threads);
+        thread_pool.emplace_back(pi_taylor_chunk, std::ref(partial_results), i, start_step, stop_step);
+    }
+
+    for (auto &t : thread_pool) {
+        t.join();
+    }
+
+    pi = kahan_sum(partial_results);
 
     std::cout << "For " << steps << ", pi value: "
         << std::setprecision(std::numeric_limits<long double>::digits10 + 1)
